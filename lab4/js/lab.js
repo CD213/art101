9c0a5a0ee0ecac04ef246d0a706e12f53396930a
CD213 <cabedavis213@gmail.com>
2024-04-22 21:42:18 -0700

    lab 4 finished

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,35 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Implementation of Chess
+// Author: Cabe Davis
+// Date:April 22 2024
 
 // Constants
+
+<pre><code>
+//Pseudocode for a computer game
+  //Game: Chess
+  //Set up game board
+      //Create a 8x8 grid
+      //Alternate the color of each square between black and white
+  //Define peices
+      //Pawns move 1 square at a time and can only take one square diagonally, can move 2 squares forward if 1st move
+      //Rook can move horizontally and vertically as far as possible, only 1 direction at a time
+      //Bishop can move diagonally as far as possibly
+      //Knights can move vertically 3 squares and horizontally 1, or 3 squares horizontally and one square vertically
+      //Queens can move horizontally, vertically, and diagonally as far as possible, only one direction at a time
+      //Kings can only move one square horizontally, vertically, or diagonally
+      //Define the value of each piece
+  //Impliment rules/Game logic
+      //Check legitimacy of player moves
+      //Reject any moves that cannot be made under the rules/the piece cannot make
+      //Create piece taking system
+      //Keep track of and check value of taken pieces
+      //Keep track of and check positioning of all pieces
+      //Keep track of check and checkmate system
+      //Make sure checks must be solved before any other move can be made
+      //Keep track of king at all times, reject any move that puts it in danger
+</code></pre>
+
+
 
 // Functions
 
