6c7fd296f342317a46aaaa9efd6870925659ff38
CD213 <cabedavis213@gmail.com>
2024-04-28 12:34:54 -0700

    finished lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - whips
+// Author: Cabe Davis
+// Date: 28 April 2024
+
+// Define Variables
+myTransport = ["Bus","Rides from Roomies", "Mazda Miata"];
+
+// create object 4 main ride
+myMainRide = {
+  make: "Mazda",
+  model: "Miata",
+  color: "Red",
+  year: 1992,
+  age: function() {
+      return 2024 - this.year;
+  }
+}
+
+document.writeln("Getting around: " + myTransport + "<br>");
+
+document.writeln ("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Constants
 
