cfbe358a17caf5684dad8ae1cb22ab6b73a73537
CD213 <cabedavis213@gmail.com>
2024-05-02 19:15:42 -0700

    lab 7 fixed

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,36 +2,24 @@
 // Author: Your Name
 // Date:
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
 function sortUserName() {
-  var userName = window.prompt("Yo. Tell me your name big dawg.");
+  var userName = window.prompt("Yo tell me your name big dawg, no capatalization:");
   console.log("userName =", userName);
+
   var nameArray = userName.split('');
   console.log("nameArray =", nameArray);
+
   var nameArraySort = nameArray.sort();
   console.log("nameArraySort =", nameArraySort);
+
   var nameSorted = nameArraySort.join('');
   console.log("nameSorted =", nameSorted);
+
   return nameSorted;
 }
-
 //output
-document.writeIn("You if your name wasn't lame: ")
-  sortUserName(), ("</br>");
+document.writeln("You if your name wasn't lame: ", 
+    sortUserName(), "</br>")
 
 // let's get this party started
 main();
