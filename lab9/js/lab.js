fe03eef7a2bee12dcd96a7a18def1aed9a4e200c
CD213 <cabedavis213@gmail.com>
2024-05-16 20:24:56 -0700

    lab 10

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,8 +3,6 @@
 // Date: 5/11/2024
 
 $("#Challenge, #Problems, #Results").on("click", "button", function() {
-  $(this).parent().toggleClass("special");
-});
+  $(this).parent().toggleClass("special");});
   $("#Challenge, #Problems, #Results").each(function() {
-    $(this).append("<button class='make-special'>Make Special</button>");
-});
+    $(this).append("<button class='make-special'>Make Special</button>");});
